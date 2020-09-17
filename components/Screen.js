ba29731253d801a4b5453ffa41f3219334a7a4f7
Charles van Lennep <charles@Charles-MacBook-Pro.local>
2020-09-17 13:15:06 +0100

    removed import no longer needed

--- components/Screen.js
+++ components/Screen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, View, Platform } from "react-native";
+import { StyleSheet, View } from "react-native";
 import Constants from "expo-constants";
 
 import colors from "../config/colors";
