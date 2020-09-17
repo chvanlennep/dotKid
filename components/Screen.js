6c42b77674a10b806706fc1cfe328c1cda9f867f
Charles van Lennep <charles@Charles-MacBook-Pro.local>
2020-09-17 13:01:28 +0100

    removed safe-area-insets

--- components/Screen.js
+++ components/Screen.js
@@ -1,35 +1,29 @@
-import React from 'react'
-import { StyleSheet, View, Platform } from 'react-native'
-import Constants from 'expo-constants';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import React from "react";
+import { StyleSheet, View, Platform } from "react-native";
+import Constants from "expo-constants";
 
-import colors from '../config/colors';
-import LowerNavBar from './LowerNavBar';
-
-
+import colors from "../config/colors";
+import LowerNavBar from "./LowerNavBar";
 
 const Screen = ({ children, style }) => {
-    const insets = useSafeAreaInsets();
-    const styles = StyleSheet.create({
-        screen: {
-            paddingTop: Platform.OS === "android" ? Constants.statusBarHeight : insets.top,
-            flex: 1,
-        },
-        view: {
-            flex: 1,
-            backgroundColor: colors.white
-        }
-    })
-    return (
-        <View style={{flex: 1}}>
-        <View style={[styles.screen, style]}>
+  const styles = StyleSheet.create({
+    screen: {
+      paddingTop: Constants.statusBarHeight,
+      flex: 1,
+    },
+    view: {
+      flex: 1,
+      backgroundColor: colors.white,
+    },
+  });
+  return (
+    <View style={{ flex: 1 }}>
+      <View style={[styles.screen, style]}>
         <View style={[styles.view, style]}>{children}</View>
         <LowerNavBar />
-        </View>
-        </View>
-    )
-}
+      </View>
+    </View>
+  );
+};
 
-export default Screen
-
-
+export default Screen;
