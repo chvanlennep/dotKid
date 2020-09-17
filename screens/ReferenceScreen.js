baa9bc4c09d345c9d43e8659615128af82583b8f
Ryan Samuels <Ryan@Ryans-MacBook.local>
2020-09-17 10:39:49 +0100

    dependencies update and initial navigation work

---
+++ screens/ReferenceScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const ReferenceScreen = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default ReferenceScreen
+
+const styles = StyleSheet.create({})
