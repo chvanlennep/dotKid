275c05585b0898d79ed152a7f621d777beb61ccb
Charles van Lennep <charles@Charles-MacBook-Pro.local>
2020-09-18 16:33:28 +0100

    object experiment

---
+++ objects/Child.js
@@ -0,0 +1,50 @@
+export default {
+  dob: {
+    name: "Date of Birth",
+    value: null,
+  },
+  sex: {
+    name: "Sex",
+    value: null,
+  },
+  gestation: {
+    name: "Gestation at Birth",
+    value: null, //to be stored in gestation days (weeks x 7 + days)
+  },
+  height: {
+    name: "Height",
+    value: null,
+  },
+  weight: {
+    name: "Weight",
+    value: null,
+  },
+  hc: {
+    name: "Head Circumference",
+    value: null,
+  },
+  dom: {
+    name: "Date of Measurement",
+    value: new Date(),
+  },
+  sbp: {
+    name: "Systolic Blood Pressure",
+    value: null,
+  },
+  dbp: {
+    name: "Diastolic Blood Pressure",
+    value: null,
+  },
+  qt: {
+    name: "QT interval",
+    value: null,
+  },
+  rr: {
+    name: "RR interval",
+    value: null,
+  },
+  cf: {
+    name: "Correction Factor",
+    value: null,
+  },
+};
