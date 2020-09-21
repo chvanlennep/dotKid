0ef392b296a56ea689fb5e0a43d187e640c1676a
Charles van Lennep <charles@charles-mbp.lan>
2020-09-21 15:36:36 +0100

    updated hompage screens with boilerplate buttons at top (no global state yet)

--- screens/NeonateHomepageScreen.js
+++ screens/NeonateHomepageScreen.js
@@ -1,50 +1,73 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState } from "react";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
 
-import NCalcScreen from '../components/NCalcScreen'
-import colors from '../config/colors'
-import Button from '../components/buttons/Button'
-import InputButton from '../components/buttons/InputButton'
-import DOBButton from '../components/buttons/DOBButton'
-import AppText from '../components/AppText'
-import routes from '../navigation/routes'
-
+import NCalcScreen from "../components/NCalcScreen";
+import colors from "../config/colors";
+import Button from "../components/buttons/Button";
+import AppText from "../components/AppText";
+import routes from "../navigation/routes";
+import DateInputButton from "../components/buttons/input/DateInputButton";
+import SexInputButton from "../components/buttons/input/SexInputButton";
 
 const NeonateHomepageScreen = ({ navigation }) => {
-    return (
-        <NCalcScreen>
+  const [dob, setDob] = useState(null);
+  const [sex, setSex] = useState(null);
+  return (
+    <ScrollView>
+      <NCalcScreen>
         <View style={styles.topContainer}>
-        <DOBButton/>
-        <InputButton name="all-inclusive">Sex</InputButton>
+          <DateInputButton
+            userLabel="Date of Birth"
+            iconName="calendar-range"
+            dateValue={dob}
+            setDateValue={setDob}
+          />
+          <SexInputButton value={sex} setValue={setSex} />
         </View>
         <View style={styles.bottomContainer}>
-        <AppText style={styles.text}> Neonate </AppText>
-        <Button onPress={() => navigation.navigate(routes.BIRTH_CENTILE)}> Birth Centile Calculator </Button>
-        <Button onPress={() => navigation.navigate(routes.NEONATE_CENTILE)}> Centile Calculator </Button>
-        <Button onPress={() => navigation.navigate(routes.ENDOTRACHEAL_TUBE_LENGTH)}> Endotracheal Tube Length Calculator </Button>
-        <Button onPress={() => navigation.navigate(routes.ENTERAL_FEED)}> Enteral Feed Calculator </Button>
-        <Button onPress={() => navigation.navigate(routes.JAUNDICE)}> Jaundice Calculator </Button>
+          <AppText style={styles.text}> Neonate </AppText>
+          <Button onPress={() => navigation.navigate(routes.BIRTH_CENTILE)}>
+            {" "}
+            Birth Centile Calculator{" "}
+          </Button>
+          <Button onPress={() => navigation.navigate(routes.NEONATE_CENTILE)}>
+            {" "}
+            Centile Calculator{" "}
+          </Button>
+          <Button
+            onPress={() => navigation.navigate(routes.ENDOTRACHEAL_TUBE_LENGTH)}
+          >
+            {" "}
+            Endotracheal Tube Length Calculator{" "}
+          </Button>
+          <Button onPress={() => navigation.navigate(routes.ENTERAL_FEED)}>
+            {" "}
+            Enteral Feed Calculator{" "}
+          </Button>
+          <Button onPress={() => navigation.navigate(routes.JAUNDICE)}>
+            {" "}
+            Jaundice Calculator{" "}
+          </Button>
         </View>
-        </NCalcScreen>
-    )
-}
+      </NCalcScreen>
+    </ScrollView>
+  );
+};
 
-export default NeonateHomepageScreen
+export default NeonateHomepageScreen;
 
 const styles = StyleSheet.create({
-    
-    bottomContainer: {
-        padding: 20,
-        marginTop: 20
-    },
-     topContainer: {
- 
-         alignSelf: "center",
-         alignItems: "center"
-     },
-     text: {
-         fontSize: 28,
-         color: colors.black,
-         marginBottom: 5
-     }
-})
+  bottomContainer: {
+    padding: 20,
+    marginTop: 20,
+  },
+  topContainer: {
+    alignSelf: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 28,
+    color: colors.black,
+    marginBottom: 5,
+  },
+});
