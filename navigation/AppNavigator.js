4dc3eadbc2ac77599deb4f571e62ea1598c7d08b
Charles van Lennep <charles@Charles-MacBook-Pro.local>
2020-10-04 17:15:03 +0100

    support for dark mode

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,50 +1,71 @@
-import React from 'react'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import { MaterialCommunityIcons } from '@expo/vector-icons'
+import React from "react";
+import { useColorScheme } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-import APLSScreen from '../screens/APLSScreen';
-import PaedsHomepageScreen from '../screens/PaedsHomepageScreen';
-import ReferenceScreen from '../screens/ReferenceScreen'
-import SettingsScreen from '../screens/SettingsScreen'
-import CalcNavigator from './CalcNavigator';
-import colors from '../config/colors';
-import ALSNavigator from './ALSNavigator';
-
+import APLSScreen from "../screens/APLSScreen";
+import PaedsHomepageScreen from "../screens/PaedsHomepageScreen";
+import ReferenceScreen from "../screens/ReferenceScreen";
+import SettingsScreen from "../screens/SettingsScreen";
+import CalcNavigator from "./CalcNavigator";
+import colors from "../config/colors";
+import ALSNavigator from "./ALSNavigator";
 
 const Tab = createBottomTabNavigator();
 
-const AppNavigator = () => (
-
-    <Tab.Navigator tabBarOptions={{ showLabel: false}}>
-    <Tab.Screen 
-    name="Calculator" 
-    component={CalcNavigator}
-    options={{
-        tabBarIcon: ({ color, size }) => 
-        <MaterialCommunityIcons name="calculator" color={color} size={size} />
-
-    }}
-     />
-    <Tab.Screen name="ALS" component={ALSNavigator} options={{
-        tabBarIcon: ({ color, size }) => 
-        <MaterialCommunityIcons name="hospital-box" color={color} size={size} />
-
-    }} 
-    />
-    <Tab.Screen name="References" component={ReferenceScreen} options={{
-        tabBarIcon: ({ color, size }) => 
-        <MaterialCommunityIcons name="book-open-variant" color={color} size={size}/>
-    }} 
-    />
-    <Tab.Screen name="Settings" component={SettingsScreen} options={{
-        tabBarIcon: ({ color, size }) => 
-        <MaterialCommunityIcons name="settings" color={color} size={size}/>
-    }} 
-     />
+const AppNavigator = () => {
+  return (
+    <Tab.Navigator tabBarOptions={{ showLabel: false }}>
+      <Tab.Screen
+        name="Calculator"
+        component={CalcNavigator}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="calculator"
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="ALS"
+        component={ALSNavigator}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="hospital-box"
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="References"
+        component={ReferenceScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="book-open-variant"
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={SettingsScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="settings" color={color} size={size} />
+          ),
+        }}
+      />
     </Tab.Navigator>
-)
+  );
+};
 
 export default AppNavigator;
-
-
-
--- navigation/CalcNavigator.js
+++ navigation/CalcNavigator.js
@@ -1,40 +1,56 @@
-import React from 'react'
-import { createStackNavigator } from '@react-navigation/stack'
-import { StackActions } from '@react-navigation/native';
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
 
-import BPScreen from '../screens/BPScreen'
-import BSAScreen from '../screens/BSAScreen'
-import PCentileScreen from '../screens/PCentileScreen'
-import ECGScreen from '../screens/ECGScreen'
-import FluidCalculatorScreen from '../screens/FluidCalculatorScreen'
-import WETFLAGScreen from '../screens/WETFLAGScreen'
-import NeonateHomepageScreen from '../screens/NeonateHomepageScreen'
-import PaedsHomepageScreen from '../screens/PaedsHomepageScreen';
-import routes from './routes';
-import BirthCentileScreen from '../screens/BirthCentileScreen';
-import NCentileScreen from '../screens/NCentileScreen'
-import EnteralFeedScreen from '../screens/EnteralFeedScreen';
-import JaundiceScreen from '../screens/JaundiceScreen';
-import APLSLogScreen from '../screens/APLSLogScreen'
+import BPScreen from "../screens/BPScreen";
+import BSAScreen from "../screens/BSAScreen";
+import PCentileScreen from "../screens/PCentileScreen";
+import ECGScreen from "../screens/ECGScreen";
+import FluidCalculatorScreen from "../screens/FluidCalculatorScreen";
+import WETFLAGScreen from "../screens/WETFLAGScreen";
+import NeonateHomepageScreen from "../screens/NeonateHomepageScreen";
+import PaedsHomepageScreen from "../screens/PaedsHomepageScreen";
+import PCentileResultsScreen from "../screens/PCentileResultsScreen";
+
+import BirthCentileScreen from "../screens/BirthCentileScreen";
+import BirthCentileResultsScreen from "../screens/BirthCentileResultsScreen";
+import NCentileScreen from "../screens/NCentileScreen";
+import EnteralFeedScreen from "../screens/EnteralFeedScreen";
+import JaundiceScreen from "../screens/JaundiceScreen";
+import NCentileResultsScreen from "../screens/NCentileResultsScreen";
 
 const Stack = createStackNavigator();
 
-const CalcNavigator = () => (
-    <Stack.Navigator mode="card" screenOptions={{headerShown: false}} >
-    <Stack.Screen name="PaedsHomepage" component={PaedsHomepageScreen} options={{headerShown: false}} />
-    <Stack.Screen name="BloodPressure" component={BPScreen} />
-    <Stack.Screen name="BSA" component={BSAScreen} />
-    <Stack.Screen name="PCentile" component={PCentileScreen} />
-    <Stack.Screen name="ECG" component={ECGScreen} />
-    <Stack.Screen name="FluidCalculator" component={FluidCalculatorScreen} />
-    <Stack.Screen name="WETFLAG" component={WETFLAGScreen} />
-    <Stack.Screen name="NeonateHomepage" component={NeonateHomepageScreen} />
-    <Stack.Screen name="BirthCentile" component={BirthCentileScreen} />
-    <Stack.Screen name="NCentile" component={NCentileScreen} />
-    <Stack.Screen name="EnteralFeed" component={EnteralFeedScreen} />
-    <Stack.Screen name="Jaundice" component={JaundiceScreen} />
+const CalcNavigator = () => {
+  return (
+    <Stack.Navigator mode="card" screenOptions={{ headerShown: false }}>
+      <Stack.Screen
+        name="PaedsHomepage"
+        component={PaedsHomepageScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen name="BloodPressure" component={BPScreen} />
+      <Stack.Screen name="BSA" component={BSAScreen} />
+      <Stack.Screen name="PCentile" component={PCentileScreen} />
+      <Stack.Screen
+        name="PCentileResultsScreen"
+        component={PCentileResultsScreen}
+      />
+      <Stack.Screen name="ECG" component={ECGScreen} />
+      <Stack.Screen name="FluidCalculator" component={FluidCalculatorScreen} />
+      <Stack.Screen name="WETFLAG" component={WETFLAGScreen} />
+      <Stack.Screen name="NeonateHomepage" component={NeonateHomepageScreen} />
+      <Stack.Screen name="BirthCentile" component={BirthCentileScreen} />
+      <Stack.Screen
+        name="BirthCentileResults"
+        component={BirthCentileResultsScreen}
+      />
+      <Stack.Screen name="NCentile" component={NCentileScreen} />
+      <Stack.Screen name="NCentileResults" component={NCentileResultsScreen} />
+      <Stack.Screen name="EnteralFeed" component={EnteralFeedScreen} />
+      <Stack.Screen name="Jaundice" component={JaundiceScreen} />
     </Stack.Navigator>
-);
+  );
+};
 
 export default CalcNavigator;
 
