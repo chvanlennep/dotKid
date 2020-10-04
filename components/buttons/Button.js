3aa446c528193123cd7c522ee099606da53409d4
Charles van Lennep <charles@Charles-MacBook-Pro.local>
2020-10-04 17:11:44 +0100

    updated buttons

--- components/buttons/Button.js
+++ components/buttons/Button.js
@@ -1,33 +1,37 @@
-import React from 'react'
-import { StyleSheet, Text, TouchableOpacity, TouchableWithoutFeedback, View } from 'react-native'
+import React from "react";
+import { StyleSheet, TouchableOpacity, View } from "react-native";
 
-import colors from '../../config/colors'
-import AppText from '../AppText'
+import colors from "../../config/colors";
+import AppText from "../AppText";
+import defaultStyles from "../../config/styles";
 
-const InputButton = ({ children, style, onPress }) => {
-    return (
-        <TouchableOpacity onPress={onPress}>
-        <View style={[ styles.button, style ]}> 
-        <AppText>{children}</AppText>
-        </View>
-        </TouchableOpacity>
-    )
-}
+const InputButton = ({
+  label,
+  style,
+  textStyle = { color: colors.white },
+  onPress,
+}) => {
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <View style={[styles.button, style]}>
+        <AppText style={textStyle}>{label}</AppText>
+      </View>
+    </TouchableOpacity>
+  );
+};
 
-export default InputButton
+export default InputButton;
 
 const styles = StyleSheet.create({
-    button: {
-        alignItems: "center",
-        backgroundColor: colors.light,
-        borderRadius: 5,
-        color: colors.white,
-        flexDirection: "row",
-        height: 57,
-        margin: 5, 
-        padding: 10,
-        width: "100%",
-        
-    },
-
-})
+  button: {
+    alignItems: "center",
+    backgroundColor: colors.light,
+    borderRadius: 5,
+    flexDirection: "row",
+    justifyContent: "center",
+    height: 57,
+    margin: 5,
+    padding: 10,
+    ...defaultStyles.container,
+  },
+});
---
+++ components/buttons/NavigateButton.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { StyleSheet, TouchableOpacity, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
+import colors from "../../config/colors";
+import defaultStyles from "../../config/styles";
+import AppText from "../AppText";
+
+const NavigateButton = ({ children, style, directions }) => {
+  const navigation = useNavigation();
+  return (
+    <TouchableOpacity
+      onPress={() => {
+        navigation.navigate(directions);
+      }}
+    >
+      <View style={[styles.button, style]}>
+        <AppText style={{ color: colors.white }}>{children}</AppText>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default NavigateButton;
+
+const styles = StyleSheet.create({
+  button: {
+    alignItems: "center",
+    backgroundColor: colors.medium,
+    borderRadius: 5,
+    color: colors.white,
+    flexDirection: "row",
+    height: 57,
+    margin: 5,
+    padding: 10,
+    width: defaultStyles.container.width - 25,
+  },
+});
