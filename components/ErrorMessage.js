4e1a1c602c15a62f6bb746495e54fb397d706d09
Charles van Lennep <charles@Charles-MacBook-Pro.local>
2020-10-04 17:13:17 +0100

    new error message component

--- components/ErrorMessage.js
+++ components/ErrorMessage.js
@@ -1,16 +1,27 @@
 import React from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet, useWindowDimensions, View } from "react-native";
 
 import AppText from "./AppText";
 
 function ErrorMessage({ error, visible }) {
+  const windowWidth = useWindowDimensions().width;
+  const buttonWidth = windowWidth - 10;
+
   if (!visible || !error) return null;
 
-  return <AppText style={styles.error}>{error}</AppText>;
+  return (
+    <View style={[styles.container, { width: buttonWidth }]}>
+      <AppText style={styles.error}>{error}</AppText>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-  error: { color: "red" },
+  container: {
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  error: { color: "red", textAlign: "center" },
 });
 
 export default ErrorMessage;
