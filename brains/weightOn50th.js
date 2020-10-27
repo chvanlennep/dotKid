5d5cae7e1e0fc60411996773f172345b15c648c0
Charles van Lennep <charles@charles-mbp.lan>
2020-10-27 17:37:10 +0000

    changed wetflag weight estimator

---
+++ brains/weightOn50th.js
@@ -0,0 +1,18 @@
+import centileData from './centileData';
+
+export default (ageInDays, ageInMonths, sex) => {
+  const index = ageInDays <= 1459 ? ageInDays : ageInMonths + 1412;
+  const array = centileData['child'][sex]['weight'][index];
+
+  function computeChildMeasurementForGraph(z, lConstant, mConstant, sConstant) {
+    return mConstant * Math.pow(z * lConstant * sConstant + 1, 1 / lConstant);
+  }
+
+  const output = computeChildMeasurementForGraph(
+    0,
+    array[1],
+    array[2],
+    array[3]
+  );
+  return output;
+};
