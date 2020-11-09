8fa6331ca8a246de773d39d7df95ff7581b215ba
Charles van Lennep <charles@charles-mbp.lan>
2020-11-09 12:38:56 +0000

    whole colour palette added

--- config/colors.js
+++ config/colors.js
@@ -1,14 +1,16 @@
+import colorPalette from './colorPalette';
+
 export default {
-  primary: "#eb5757", //pink
-  darkPrimary: "#B32425", //dark red
-  lightPrimary: "#ED9B9C",
-  secondary: "#219653", //green
-  darkSecondary: "#096534", //dark green
-  lightSecondary: "#C4E9D1",
-  black: "#000",
-  white: "#fff",
-  medium: "#828282",
-  light: "#c4c4c4",
-  dark: "#4f4f4f",
-  danger: "#ff5252",
+  primary: colorPalette.primary.default, //pink
+  darkPrimary: colorPalette.primary[900], //dark red
+  lightPrimary: '#ED9B9C',
+  secondary: colorPalette.secondary.default, //green
+  darkSecondary: colorPalette.secondary[900], //dark green
+  lightSecondary: '#C4E9D1',
+  black: '#000',
+  white: '#fff',
+  medium: '#828282',
+  light: '#c4c4c4',
+  dark: '#4f4f4f',
+  danger: '#ff5252',
 };
